49eb5404819476231b5324e19104f99b36ca4c9e
Samuel Meenzen <samuel@meenzen.net>
2023-10-14 21:03:42 +0200

    chore: update cloudflare build config

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -5,4 +5,4 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet workload install wasm-tools
-./dotnet/dotnet publish -c Release -o output
+./dotnet/dotnet publish -c Release
