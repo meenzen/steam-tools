9e0514fb2f1b5a8080fa69a82a4d3786fc6a7749
Samuel Meenzen <samuel@meenzen.net>
2023-11-01 14:23:47 +0100

    feat: use proxy to directly call the steam api

--- src/Meenzen.SteamTools/Services/SteamApi.cs
+++ src/Meenzen.SteamTools/Services/SteamApi.cs
@@ -1,43 +1,42 @@
-using Microsoft.JSInterop;
+using System.Net.Http.Json;
+using Meenzen.SteamTools.Generated;
+using Meenzen.SteamTools.Models;
 using MudBlazor;
 
 namespace Meenzen.SteamTools.Services;
 
 public class SteamApi
 {
-    private readonly IJSRuntime _jsRuntime;
     private readonly ISnackbar _snackbar;
+    private readonly HttpClient _client;
 
-    public SteamApi(IJSRuntime jsRuntime, ISnackbar snackbar)
+    public SteamApi(ISnackbar snackbar, HttpClient client)
     {
-        _jsRuntime = jsRuntime;
         _snackbar = snackbar;
+        _client = client;
     }
 
     public string ApiKey { get; set; } = string.Empty;
     public ulong SteamId { get; set; } = 0;
     public bool IsConfigured => !string.IsNullOrWhiteSpace(ApiKey) && SteamId != 0;
 
-    private async Task OpenTabAsync(string url)
-    {
-        await _jsRuntime.InvokeVoidAsync("open", url, "_blank");
-    }
-
     private void ShowConfigurationSnackbar()
     {
         _snackbar.Add("Please configure the Steam API key and Steam ID first.", Severity.Warning);
     }
 
-    public Task GetOwnedGamesAsync()
+    public async Task<SteamResponse<OwnedGamesResponse>?> GetOwnedGamesAsync()
     {
         if (!IsConfigured)
         {
             ShowConfigurationSnackbar();
-            return Task.CompletedTask;
+            return null;
         }
 
-        return OpenTabAsync(
-            $"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={ApiKey}&steamid={SteamId}"
+        SteamResponse<OwnedGamesResponse>? result = await _client.GetFromJsonAsync(
+            $"IPlayerService/GetOwnedGames/v1/?key={ApiKey}&steamid={SteamId}",
+            SourceGenerationContext.Default.SteamResponseOwnedGamesResponse
         );
+        return result;
     }
 }
