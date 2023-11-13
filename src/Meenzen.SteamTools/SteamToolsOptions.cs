aad538384e13253959f0490909dcae190d079eb1
Samuel Meenzen <samuel@meenzen.net>
2023-11-13 18:51:34 +0100

    feat: inform users about the app deployment
    closes #6, closes #7

--- src/Meenzen.SteamTools/SteamToolsOptions.cs
+++ src/Meenzen.SteamTools/SteamToolsOptions.cs
@@ -4,4 +4,7 @@ public class SteamToolsOptions
 {
     public required Uri BaseAddress { get; set; }
     public required Uri ProxyUri { get; set; }
+
+    public bool IsOfficialDeployment => BaseAddress.Host.EndsWith("mnzn.dev");
+    public bool IsPreviewDeployment => BaseAddress.Host.EndsWith("steam-tools-8yd.pages.dev");
 }
