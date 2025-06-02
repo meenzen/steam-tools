a224df5b2452ee4f2a1e01e94677e7b9f47f7292
Samuel Meenzen <samuel@meenzen.net>
2025-06-02 13:19:56 +0200

    feat: add app.js file

---
+++ src/Meenzen.SteamTools/wwwroot/js/app.js
@@ -0,0 +1,6 @@
+console.log(
+    "%cSteamTools\n" +
+    `%cCopyright (c) 2023-${new Date().getFullYear()} Samuel Meenzen\n`+
+    "https://github.com/meenzen/steam-tools",
+    "color: #1474a9; font-size: 80px;", "font-size: 18px;"
+);
