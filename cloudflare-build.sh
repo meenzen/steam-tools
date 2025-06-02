29d2a1e0e050f91f1d413b543be73bfb9272f4d3
Samuel Meenzen <samuel@meenzen.net>
2025-06-02 12:41:21 +0200

    feat: use asset fingerprinting

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -2,7 +2,7 @@
 git fetch --unshallow
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet publish -c Release
